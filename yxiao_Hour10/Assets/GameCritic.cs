8ed928063ae18193b09090092034cf5a2d51eb4c
hyperLink <159730875+yuzu-cs@users.noreply.github.com>
2026-04-26 19:54:20 -0700

    Complete 3 enhancements

--- yxiao_Hour10/Assets/GameCritic.cs
+++ yxiao_Hour10/Assets/GameCritic.cs
@@ -1,1 +1,22 @@
-/* text goes here! */
+/* 
+ * ==============================================================================
+ * 1: Changed Footstep Sounds to Water Footsteps
+ *       
+ *      Added a Walking in Water.ogg to the Audio folder. In the FPSController, replaced the original footstep audio with this new water sound.
+ *      The player now hears water splashing sounds when walking, which improves immersion and fits the scene theme
+ *      
+ * ==============================================================================
+ * 2: Modified Player Bumper Shape and Position
+ * 
+ *      Resized the player's bumper to (5.5, 2, 1) to make it longer and narrower. 
+ *      Adjusted Position Y=1 to lift it off the ground so it appears as a floating board
+ * 
+ * ==============================================================================
+ * 3: Added a Timer
+ * 
+ *      Added elapsedTime variable to track gameplay time. Timer runs during gameplay and stops when all goals are solved. 
+ *      Current time shows on screen during gameplay, final time shows on the Game Over screen.
+ *      
+ * ==============================================================================
+ * 
+ */
--- yxiao_Hour10/Assets/Scripts/GameManager.cs
+++ yxiao_Hour10/Assets/Scripts/GameManager.cs
@@ -1,40 +1,41 @@
-//using System.Collections;
-//using System.Collections.Generic;
-//using UnityEngine;
-
-//public class GameManager : MonoBehaviour
-//{
-//    // Start is called before the first frame update
-//    void Start()
-//    {
-        
-//    }
-
-//    // Update is called once per frame
-//    void Update()
-//    {
-        
-//    }
-//}
 using UnityEngine;
+
 public class GameManager : MonoBehaviour
 {
     public GoalScript blue, green, red, orange;
-    private bool isGameOver = true;
-    void Update()
+		
+	private float elapsedTime = 0;
+	private bool isFinished = false;
+
+	void Update()
     {
-        // If all four goals are solved then the game is over
-        isGameOver = blue.isSolved && green.isSolved && red.isSolved && orange.isSolved;
-    }
-    void OnGUI()
+		// If all four goals are solved then the game is over
+		isFinished = blue.isSolved && green.isSolved && red.isSolved && orange.isSolved;
+
+		if (!isFinished)
+		{
+			elapsedTime = elapsedTime + Time.deltaTime;
+		}
+	}
+
+	
+	void OnGUI()
     {
-        if (isGameOver)
+        if (isFinished)
         {
             Rect rect = new Rect(Screen.width / 2 - 100, Screen.height / 2 - 50, 200, 75);
             GUI.Box(rect, "Game Over");
             Rect rect2 = new Rect(Screen.width / 2 - 30, Screen.height / 2 - 25, 60, 50);
             GUI.Label(rect2, "Good Job!");
-        }
 
-    }
+			GUI.Box(new Rect(Screen.width / 2 - 65, 185, 130, 40), "Your Time Was");
+			GUI.Label(new Rect(Screen.width / 2 - 10, 200, 20, 30), ((int)elapsedTime).ToString());	
+		}
+		else
+		{
+			GUI.Box(new Rect(Screen.width / 2 - 65, Screen.height - 115, 130, 40), "Your Time Is");
+			GUI.Label(new Rect(Screen.width / 2 - 10, Screen.height - 100, 20, 30), ((int)elapsedTime).ToString());
+		}
+		
+	}
 }
