ab94d01081031614c20a11de95458731b849ff16
hyperLink <159730875+yuzu-cs@users.noreply.github.com>
2026-04-28 19:46:43 -0700

    game critic

--- yxiao_Hour10/Assets/GameCritic.cs
+++ yxiao_Hour10/Assets/GameCritic.cs
@@ -1,4 +1,33 @@
-/* 
+﻿/*_____                       _____      _ _   _      
+ / ____|                     / ____|    (_) | (_)     
+| |  __  __ _ _ __ ___   ___| |     _ __ _| |_ _  ___ 
+| | |_ |/ _` | '_ ` _ \ / _ \ |    | '__| | __| |/ __|
+| |__| | (_| | | | | | |  __/ |____| |  | | |_| | (__ 
+ \_____|\__,_|_| |_| |_|\___|\_____|_|  |_|\__|_|\___|
+ * 
+ * ► Is the game too easy or hard?
+ *   a: The original game is fairly easy because the player can literally stay where they are from the beginning 
+ *      and just wait for all the goals to be solved automatically purely by luck. 
+ *      The chaos ball doesn't really cause too much chaos. Instead, it energizes the colored balls to make it
+ *      easier for them to get to their goals
+ *   
+ * ► What would make it easier or harder?
+ *   a: It would be harder if the goal is only solved when it is a straight hit from the bumper. Also, if the 
+ *      chaos ball does something like drawing damages to the player etc., this would also be harder.
+ *   
+ * ► What would give it a “wow” factor?
+ *   a: The chaos ball isn't really chaotic enough. If it does more damages or causes more chaos, or if it gets 
+ *      a more interesting look, it will be a "wow" factor probably. 
+ *      If we can alter the speed and viewing angle/depth of the player, the gameplay can be more fun too.
+ *      
+ * ► What parts of the game are fun? What parts of the game are tedious?
+ *   a: I like the physics and collision. The fact that they are bouncing off one another randomly is both complex
+ *      and fun. 
+ *      One tedious part I obviously found is that the player can't do a lot in this game.
+ *   
+ *   
+ *   
+ *   
  * ==============================================================================
  * 1: Changed Footstep Sounds to Water Footsteps
  *       
