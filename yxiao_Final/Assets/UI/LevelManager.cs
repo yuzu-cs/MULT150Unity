66d24c2041b2421c5be87c5d68d7b0649d88faa2
hyperLink <159730875+yuzu-cs@users.noreply.github.com>
2026-05-28 17:30:26 -0700

    completeUI

---
+++ yxiao_Final/Assets/UI/LevelManager.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class LevelManager : MonoBehaviour
+{
+	public string sceneToLoad = "yxiao_Final";
+
+
+	public void LoadGame ()
+	{
+		SceneManager.LoadScene(sceneToLoad);
+	}
+}
